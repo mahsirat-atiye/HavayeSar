2a5dca35dd89a63575b3b464932ed11b138df9e3
mahsirat-atiye <atieh.atieh.jamshidpoor@gmail.com>
2020-03-01 00:21:24 +0330

    merging tables

--- test.py
+++ test.py
@@ -1,1 +1,8 @@
-print("test from other user")
+import pandas as pd
+import numpy as np
+ad = pd.read_csv("datadays2020_contest_public_dataset/ad.csv")
+ad_image = pd.read_csv("datadays2020_contest_public_dataset/ad_image.csv")
+
+result = pd.merge(ad, ad_image, on='adId')
+
+print(result.head())
