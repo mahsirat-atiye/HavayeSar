8079d2f90d6fcb245f4ff22c238c95fe2527325a
mahsirat-atiye <atieh.atieh.jamshidpoor@gmail.com>
2020-03-01 03:13:24 +0330

    reformating page views

--- test.py
+++ test.py
@@ -3,33 +3,36 @@ import json
 import numpy as np
 # reformating data
 page_topic = pd.read_csv("datadays2020_contest_public_dataset/page_topic.csv")
-print(page_topic.head())
-new_page_topic =[]
+# print(page_topic.head())
+new_page_topic_list =[]
 current_row = dict()
 current_pageId = -1
 for index, row in page_topic.iterrows():
-    print(row['pageId'], row['topicId'])
+    # print(row['pageId'], row['topicId'])
     if current_pageId != int(row['pageId']):
-        new_page_topic.append(current_row)
+        new_page_topic_list.append(current_row)
         current_row = dict()
         current_pageId = row['pageId']
         current_row['pageId'] = current_pageId
         current_row[str(int(row['topicId']))] = row['confidence']
     else:
         current_row[str(int(row['topicId']))] = row['confidence']
-dataform = pd.read_json(str(json.dumps(new_page_topic)))
-dataform.fillna(value=0, inplace=True)
-print(dataform.head())
-dataform.to_csv(path_or_buf='a.csv', index=False)
+new_page_topic = pd.read_json(str(json.dumps(new_page_topic_list)))
+new_page_topic.fillna(value=0, inplace=True)
+# print(new_page_topic.head())
+# new_page_topic.to_csv(path_or_buf='a.csv', index=False)
 
 
 
 # =======================================================================
+# reformating data
+
+user_page_view =pd.read_csv("datadays2020_contest_public_dataset/user_page_view.csv")
+new_user_page_view = (user_page_view.groupby(['userId', 'pageId'], sort=False).sum())
+
+print(new_user_page_view.head())
 
 
-
-
-
-# result = pd.merge(ad, ad_image, on='adId')
-#
+# =================================================
+result = pd.merge(new_user_page_view[["userId", "pageId", "timestamp"]], new_page_topic, on='pageId')
 # print(result.head())
